618561ef7fab16d8fdb045b96d6f131a642b2fbf
Hypersaki <146135547+Hypersaki@users.noreply.github.com>
2023-11-16 11:23:33 +0000

    Add files via upload

---
+++ test/Connecting_to_Wifi/Connecting_to_Wifi.ino
@@ -0,0 +1,36 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "SSID here";
+const char* password = "password here";
+const char* host = "iot.io";
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+}
