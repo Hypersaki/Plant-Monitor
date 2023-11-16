a001b6a751d72e8717580207939086e3b78d4a6c
Hypersaki <146135547+Hypersaki@users.noreply.github.com>
2023-11-16 11:40:33 +0000

    Update and rename arduino_secrets.h to DHT22_MQTT with nails/arduino_secrets.h

---
+++ DHT22_MQTT with nails/arduino_secrets.h
@@ -0,0 +1,8 @@
+#define SECRET_SSID "asdoigjhaosdpiguhaosp" 
+#define SECRET_PASS "djaslkfjalk;shgd;lksdh"
+//wifi name and password
+//it's deleted right here
+#define SECRET_MQTTUSER "glkajh;gkdjsafg;lsakdf"
+#define SECRET_MQTTPASS "gadfhglaksjdfklsagasd"
+//MQTT user name and password
+//it's deleted right here
--- arduino_secrets.h
+++
@@ -1,6 +0,0 @@
-#define SECRET_SSID "CE-Hub-Student" 
-#define SECRET_PASS "casa-ce-gagarin-public-service"
-//wifi name and password
-#define SECRET_MQTTUSER "student"
-#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"
-//MQTT user name and password
